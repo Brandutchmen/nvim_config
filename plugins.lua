aa52a9bf8198964921168ae24f0959e5f1ee898e
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2023-08-18 01:17:37 -0500

    Copilot fix

--- plugins.lua
+++ plugins.lua
@@ -59,58 +59,8 @@ local plugins = {
         vim.cmd [[ do User LspAttachBuffers ]]
       end)
     end,
-
-    ["zbirenbaum/copilot.lua"] = {
-      cmd = "Copilot",
-      event = { "InsertEnter", "BufEnter", "BufRead" },
-      config = function()
-        require("copilot").setup {
-          panel = {
-            enabled = true,
-            auto_refresh = true,
-            keymap = {
-              jump_prev = "[[",
-              jump_next = "]]",
-              accept = "<CR>",
-              refresh = "gr",
-              open = "<M-CR>",
-            },
-            layout = {
-              position = "bottom", -- | top | left | right
-              ratio = 0.4,
-            },
-          },
-          suggestion = {
-            enabled = true,
-            auto_accept = true,
-            auto_trigger = true,
-            debounce = 75,
-            keymap = {
-              accept = "<Tab>",
-              accept_word = false,
-              accept_line = false,
-              next = "<M-]>",
-              prev = "<M-[>",
-              dismiss = "<C-]>",
-            },
-          },
-          filetypes = {
-            yaml = false,
-            markdown = false,
-            help = false,
-            gitcommit = false,
-            gitrebase = false,
-            hgcommit = false,
-            svn = false,
-            cvs = false,
-            ["."] = false,
-          },
-          copilot_node_command = "node", -- Node.js version must be > 16.x
-          server_opts_overrides = {},
-        }
-      end,
-    },
-    ["m4xshen/autoclose.nvim"] = {
+  },
+   ["m4xshen/autoclose.nvim"] = {
       event = "BufEnter",
       config = function()
         require("autoclose").setup()
@@ -129,11 +79,11 @@ local plugins = {
     --   "mg979/vim-visual-multi",
     --   lazy = false,
     -- }
-    -- {
-    --   "zbirenbaum/copilot.lua",
-    --   event = "InsertEnter",
-    --   opts = overrides.copilot,
-    -- },
+    {
+      "zbirenbaum/copilot.lua",
+      event = "InsertEnter",
+      opts = overrides.copilot,
+    },
     {
       "hrsh7th/nvim-cmp",
       dependencies = {
@@ -157,7 +107,6 @@ local plugins = {
     },
     -- To use a extras plugin
     -- { import = "custom.configs.extras.symbols-outline", },
-  },
 }
 
 return plugins
