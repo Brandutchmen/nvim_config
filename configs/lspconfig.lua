6e43d8e1086f54e64f3278b1e03ca159535050ec
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2023-08-17 19:16:58 -0500

    Added php properly

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "php", "gopls", "pyright", "yamlls", "dockerls", "cmake", "terraformls", "vimls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "phpactor", "astro", "gopls", "pyright", "yamlls", "dockerls", "cmake", "terraformls", "vimls" }
 
 for _, lsp in ipairs(servers) do
   local config = {
